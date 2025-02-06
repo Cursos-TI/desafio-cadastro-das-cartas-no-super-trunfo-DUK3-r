80e3cab23c2480322031f5ff2db6f22e17ce8ead
DUK3-r <vittu201201@gmail.com>
2025-02-06 22:56:52 +0000

    escrevi o código completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,18 +5,53 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
+#include <stdio.h>
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+/*UTILIZADO AS FUNÇÕES ENSINADAS EM AULA PELO PROFESSOR SERGIO PARA APLICAÇÃO DESSA ATIVIDADE!!!
+COM ALGUMAS MELHORIAS CONHECIDAS E OUTRAS QUE PESQUISEI NA INTERNET PARA PODER DEIXAR O PROGRAMA MAIS CLARO!!!
+*/
+int main()
+{
+    // VARIAVEIS SALVAS SEM CONTEUDO PARA PREENCHER AS INFORMAÇÕES NO FORMULARIO
+    char codigo_cidade[3];
+    char nome[50];
+    double populacao;
+    double area;
+    double PIB;
+    int pontos_turisticos;
 
+    // FORMULARIO PARA COLETAR OS DADOS DOS USUARIOS (ENTRADA DE DADOS)
+    printf("Bem Vindo ao SuperTrunfo - Paises\n");
+    printf("Cadastre a nova carta do SuperTrunfo\n");
+
+    printf("Digite o codigo da cidade: ");
+    scanf("%2s", codigo_cidade);
+    while (getchar() != '\n');  // Limpa o buffer para evitar problemas na leitura de fgets
+
+
+    printf("Digite o nome da cidade: ");
+    fgets(nome, 50, stdin);// UTILIZANDO FGETS PARA CASO O PAIS TENHA ESPAÇO ENTRE OS NOMES
+
+    printf("Populacao: "),
+    scanf("%lf", &populacao);
+
+    printf("Qual a area (m2): ");
+    scanf("%lf", &area);
+
+    printf("Qual o PIB: ");
+    scanf("%lf", &PIB);
+
+    printf("Quantidade de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+
+    // RESULTADO DA ENTRADA DE DADOS (SAIDA DE DADOS)
+    printf("\n\nInformacoes da Carta - %s %s \n", codigo_cidade, nome);
+
+    printf("Populacao: %.2lf\n", populacao);
+    printf("Area: %.2lfm2\n", area);
+    printf("PIB: %.2lf\n", PIB);
+    printf("Pontos turisticos: %d\n", pontos_turisticos);
+    //UTIIZEI .2 PARA DETERMINAR A QUANTIDADE DE DECIMAIS
     return 0;
 }
+
